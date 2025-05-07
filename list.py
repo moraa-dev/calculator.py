fa7a80f791bb06a6e6636aff9d9d84b664f25960
moraa-dev <a.moraa@iscodkenya.org>
2025-05-07 17:05:07 +0300

    Update list.py

--- list.py
+++ list.py
@@ -1,31 +1,16 @@
-# Create an empty list called my_list.
-my_list = []
+my_list = []  # Initializing my_list
 
-# Append the following elements to my_list: 10, 20, 30, 40.
+my_list.append(10) #appending value 10 to my_list
+my_list.append(20) #appending value 20 to my_list
+my_list.append(30) #appending value 30 to my_list
+my_list.append(40) #appending value 40 to my_list
 
-my_list.append (10)
-print(my_list)
+my_list.insert(2, 15)  #inserting value 15 at index 2
+my_list.extend([50, 60, 70]) #extending my_list with elements 50, 60, 70
+my_list.pop(7) #removing element at index 7
+my_list.sort() #sorting the my_list in ascending order
 
-my_list.append (20)
-print(my_list)
+print(my_list) #displaying the results
 
-my_list.append (30)
-print(my_list)
-
-my_list.append (40)
-print(my_list)
-
-my_list.insert(2, 15)  # Insert 15 at index 2
-print(my_list)
-
-my_list.extend ([50, 60, 70]) # Extend my_list with another list: [50, 60, 70]
-print(my_list)
-
-my_list.pop (7) # Remove the last element from my_list.
-print(my_list)
-
-my_list.sort () # Sort my_list in ascending order.
-print(my_list)
-
-index_of_30 = my_list.index(30)  # Find and print the index of the value 30 in my_list.
-print (index_of_30)
+my_list_30 = my_list.index(30) #finding element 30 via index
+print (my_list_30) #displaying the value of element in index 
